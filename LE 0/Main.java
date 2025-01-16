daa6a3c49815d7eb6d5172b9c3a90badf2fa3c0b
Pratham-0001 <221B497@juetguna.in>
2025-01-16 15:04:05 +0530

    Update Main.java

--- LE 0/Main.java
+++ LE 0/Main.java
@@ -3,7 +3,7 @@
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Yashasvi Grover");
+		System.out.println("Prathamesh");
 		int x=Integer.parseInt(args[0]);
 		for(int i=0;i<=10;i++){
 		    System.out.println(x +" * "+i +" = "+x*i);
